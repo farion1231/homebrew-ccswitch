c7d05e1ec89163f0fc74cf207bc14dca7c96b5b0
Jason <farion1231@gmail.com>
2025-10-13 09:57:28 +0800

    feat: initial Homebrew cask for CC Switch v3.5.0

---
+++ Casks/cc-switch.rb
@@ -0,0 +1,29 @@
+cask "cc-switch" do
+  version "3.5.0"
+  sha256 "387ab64f6d7a09a463015a7ae3d3492e6f3e7226975a5633be154283d4684b77"
+
+  url "https://github.com/farion1231/cc-switch/releases/download/v#{version}/CC-Switch-v#{version}-macOS.tar.gz",
+      verified: "github.com/farion1231/cc-switch/"
+  name "CC Switch"
+  desc "Configuration manager for Claude Code and Codex providers"
+  homepage "https://github.com/farion1231/cc-switch"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "CC Switch.app"
+
+  zap trash: [
+    "~/.cc-switch",
+    "~/Library/Application Support/com.ccswitch.desktop",
+    "~/Library/Caches/com.ccswitch.desktop",
+    "~/Library/Preferences/com.ccswitch.desktop.plist",
+    "~/Library/Saved Application State/com.ccswitch.desktop.savedState",
+    "~/Library/WebKit/com.ccswitch.desktop",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+# Homebrew Tap for CC Switch
+
+## Installation
+
+```bash
+brew tap farion1231/ccswitch
+brew install --cask cc-switch
+```
+
+## Updating
+
+```bash
+brew update
+brew upgrade --cask cc-switch
+```
+
+## Uninstallation
+
+```bash
+brew uninstall --cask cc-switch
+brew untap farion1231/ccswitch
+```
